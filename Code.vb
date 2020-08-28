de298ae67df6d169c6085900b0e201d979ecbdbc
nletcher <sofiamwaliza@gmail.com>
2020-08-27 22:49:23 -0700

    first commit

---
+++ Code.vb
@@ -0,0 +1,20 @@
+Sub Program()
+'Declare variables
+Dim Ticker As String
+Dim Opening_price As Double
+Dim Closing_price As Long
+Dim Percentage_change As Double
+Dim Yearly_change As Double
+
+Dim Total As LongLong
+
+Dim i As Long
+Dim Lastrow As Long
+Dim Summary_table_row As Integer
+Dim Max As Long
+Dim Min As Long
+Dim Max_Volume As LongLong
+Dim No_tickers As Long
+Dim Max_ticker As Long
+Dim Min_ticker As Long
+Dim Max_Volume_ticker As Long
