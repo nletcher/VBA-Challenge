1619561d86cdfec0d7f3f48847f1178f200ee858
nletcher <sofiamwaliza@gmail.com>
2020-08-29 18:03:41 -0700

    For Loop

--- Code.vb
+++ Code.vb
@@ -37,4 +37,45 @@ Range("Q3").Value = "Greatest % Increase"
 Range("Q4").Value = "Greatest % Decrease"
 Range("Q5").Value = "Greatest Total Volume"
 
+'For Loop for all rows
+For i = 2 To Lastrow
+
+    If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
+        Ticker = Cells(i, 1).Value
+        Total = Total + Cells(i, 7).Value
+        Opening_price = Cells(i, 3).Value
+        
+        Closing_price = Cells(i, 6).Value
+        
+        Yearly_change = (Closing_price - Opening_price)
+        
+        If Opening_price = 0 Then
+            Percentage_change = 0
+        Else
+        Percentage_change = (Yearly_change / Opening_price) * 100
+        End If
+        If Yearly_change >= 0 Then
+            Range("M" & Summary_table_row).Interior.ColorIndex = 4
+            
+        Else
+            Range("M" & Summary_table_row).Interior.ColorIndex = 3
+        
+        End If
+        
+
+        Range("L" & Summary_table_row).Value = Ticker
+        Range("M" & Summary_table_row).Value = Yearly_change
+        Range("N" & Summary_table_row).Value = Percentage_change
+        Range("O" & Summary_table_row).Value = Total
+        
+        Summary_table_row = Summary_table_row + 1
+        Total = 0
+    Else
+    
+        Total = Total + Cells(i, 7).Value
+    
+    
+    End If
+    
+Next i
 
